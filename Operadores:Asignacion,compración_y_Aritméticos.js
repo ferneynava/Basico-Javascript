ef4fd72891b3558e0db135450d96930992992044
Ferney Nava <ferneyalexander_12@hotmail.com>
2021-09-05 13:22:13 -0500

    Operadores de asignación de anulación lógica
    Nuevos operadores

--- Operadores:Asignacion,compración_y_Aritméticos.js
+++ Operadores:Asignacion,compración_y_Aritméticos.js
@@ -12,6 +12,7 @@
 !false; //devolverá true
 //Operador de asignación
 var a = 1; //= asignando el valor a la variable 
+
 //operador de comparadión: 
 3 == "3"; // va a comparar el valor mas no el tipo 
 3 === "3"; //Compara que el valor y el tipo sean iguales
@@ -29,3 +30,23 @@ a || b; // va a validar que solo a o b sea verdad para que se cumpla esa condici
 var edad = 40;
 edad++; // el valor de edad se va incrementar en uno
 edad += 2; //el valor de dad va incrementar en dos. 
+
+//operador de asignación de anulación lógica
+null ?? 5 // => 5 // ?? Operador coalescente nulo (??) Es un operador lógico que devuelve su operando del lado 
+                  // derecho cuando su operando del lado izquiero es null o undefined, y de lo contrario devuelve 
+                  // su operador del lado izquierdo. 
+const nullValue = null;
+const emptyText = ""; // falsy
+const someNumber = 42;
+
+const valA = nullValue ?? "default for A";
+const valB = emptyText ?? "default for B";
+const valC = someNumber ?? 0;
+
+console.log(valA); // "default for A"
+console.log(valB); // "" (as the empty string is not null or undefined)
+console.log(valC); // 42
+
+
+null ??= 5 // => 5 // ??= Es un operador lógico que devuelve su operando del lado 
+                   // derecho cuando su operando del lado izquiero es null o undefined
